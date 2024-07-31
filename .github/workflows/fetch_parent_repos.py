39524dbcd4c59daec6cad44c8ad7708921f5265f
Somesh Sharma <160464815+sosh-odoo@users.noreply.github.com>
2024-07-31 16:26:43 +0530

    Update fetch_parent_repos.py

--- .github/workflows/fetch_parent_repos.py
+++ .github/workflows/fetch_parent_repos.py
@@ -47,7 +47,7 @@ def main():
     token = os.environ["GITHUB_TOKEN"]
     repos = search_repositories(token, query)
     print(os.environ)
-    submodule_name = os.environ["GITHUB_REPO"].split("/")[-1].replace(".git", "")
+    submodule_name = os.environ["GITHUB_REPOSITORY"].split("/")[-1].replace(".git", "")
 
     with open('parent_repos.txt', 'w') as file:
         for repo in repos:
