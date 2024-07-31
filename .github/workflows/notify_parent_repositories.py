af089e1521e7021f8402d81737fff06514856ae9
Somesh Sharma <160464815+sosh-odoo@users.noreply.github.com>
2024-07-31 15:20:49 +0530

    Update notify_parent_repositories.py

--- .github/workflows/notify_parent_repositories.py
+++ .github/workflows/notify_parent_repositories.py
@@ -19,7 +19,7 @@ def get_git_request(url,token):
     
 def main():
     token = os.environ["GITHUB_TOKEN"]
-    url = "https://api.github.com/sosh-odoo/repos"
+    url = "https://api.github.com/user/repos"
     get_git_request(url, token)
 
 if __name__ == "__main__":
