21ba5cb4a8893d2a4fff7357221c47c8153061d8
Somesh Sharma <160464815+sosh-odoo@users.noreply.github.com>
2024-07-31 16:23:00 +0530

    Update fetch_parent_repos.py

--- .github/workflows/fetch_parent_repos.py
+++ .github/workflows/fetch_parent_repos.py
@@ -1,4 +1,5 @@
 import os, requests
+from github import Github
 
 # Constants
 GITHUB_API_URL = "https://api.github.com"
@@ -6,7 +7,7 @@ SUBMODULE_REPO = "test"
 
 def search_repositories(token, query):
     """Search GitHub repositories using a query."""
-    url = f"https://api.github.com/search/repositories?q={query}"
+    url = f"{GITHUB_API_URL}/search/repositories?q={query}"
     headers = {
         "Authorization": f"token {token}",
         "Accept": "application/vnd.github.v3+json",
@@ -15,21 +16,43 @@ def search_repositories(token, query):
     
     if response.status_code == 200:
         repos =  response.json()["items"]
-        print(repos)
         return repos
     else:
         print(f"Failed to search repositories: {response.status_code} - {response.text}")
         return []
 
+def search_submodule(github_token, repo_owner, repo_name, submodule_name):
+    g = Github(github_token)
+    repo = g.get_repo(f"{repo_owner}/{repo_name}")
+    contents = repo.get_contents("")
+    submodule_found = False
+
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == 'dir':
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            if file_content.size <= 100:
+                if file_content.raw_data.get('type') == 'submodule':
+                    if file_content.raw_data.get("submodule_git_url") is not None:
+                        src = file_content.raw_data.get("submodule_git_url").split("/")[-1].replace(".git", "")
+                        if src == submodule_name:
+                            submodule_found = True
+                            break
+
+    return submodule_found
+
 def main():
-    # Replace this query with an appropriate one for finding repositories
     query = f'"{SUBMODULE_REPO}"'
     token = os.environ["GITHUB_TOKEN"]
     repos = search_repositories(token, query)
-    
+    submodule_name = os.environ["GITHUB_REPO"].split("/")[-1].replace(".git", "")
+
     with open('parent_repos.txt', 'w') as file:
         for repo in repos:
-            file.write(f"{repo['full_name']}\n")
+            repo_owner, repo_name = repo["full_name"].split("/")
+            if search_submodule(token, repo_owner, repo_name, submodule_name):
+                file.write(f"{repo['full_name']}\n")
 
 if __name__ == "__main__":
     main()
