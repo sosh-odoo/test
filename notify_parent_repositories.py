cd9e0f0cd02adf716658e1edf6131746c7f06e5b
Somesh Sharma <160464815+sosh-odoo@users.noreply.github.com>
2024-08-06 12:57:45 +0530

    Update notify_parent_repositories.py

--- notify_parent_repositories.py
+++ notify_parent_repositories.py
@@ -17,7 +17,7 @@ headers = {
 
 # Fetch repositories
 def fetch_repositories():
-    response = requests.get("https://api.github.com/users/sosh-odoo/repos", headers=headers, params={"affiliation": "owner,collaborator,organization_member"})
+    response = requests.get("https://api.github.com/users/sosh-odoo/repos", headers=headers)
     response.raise_for_status()
     repos = [repo['full_name'] for repo in response.json()]
     repos = ['sosh-odoo/test2']
